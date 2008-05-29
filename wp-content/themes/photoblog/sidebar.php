869e7572576f70a1684195c7a1621b6391442fc9
Calabacina XXX <calabacinaxxxx@powerbook-g4-17-de-calabacina-xxx-2.local>
2008-05-29 20:42:36 +0200

    sidebar.php añadido en theme photoblog

---
+++ wp-content/themes/photoblog/sidebar.php
@@ -0,0 +1,14 @@
+
+<!-- begin sidebar -->
+<div id="menu">
+
+<ul>
+<?php   /* Widgetized sidebar, if you have the plugin installed. */
+        if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar() ) :
+        // Nada aqui, vas a configurarlo del panel de widgets en el administration del blog
+        endif; ?>
+
+</ul>
+
+</div>
+<!-- end sidebar -->
